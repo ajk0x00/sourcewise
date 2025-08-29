933243a9d174671cfe08426d6f5e6b1691441d6f
ajk0x00 <ajk0x00@gmail.com>
2025-08-29 05:10:17 +0000

    Add new components to sidebar.

--- src/fragments/Sidebar.jsx
+++ src/fragments/Sidebar.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import PrimaryButton from '../components/PrimaryButton';
 import IconButton from '../components/IconButton';
 import UploadedDoc from '../components/UploadedDoc';
+import PreviousChat from '../components/PreviousChat';
+import SecondaryButton from '../components/SecondaryButton';
 import { GrAdd } from 'react-icons/gr';
 import { SlCloudUpload } from "react-icons/sl";
+import { RiSettings3Line } from "react-icons/ri";
+import girlProfile from '../assets/girlProfile.png';
 
 const Sidebar = () => {
+
+  const settingsIcon = () => (
+    <div className='flex items-center justify-center p-2 w-fit h-fit rounded-full bg-zinc-100'>
+      <RiSettings3Line />
+    </div>
+  )
+
+  const profilePic = () => (
+    <div className='w-7 h-7 rounded-full'>
+      <img src={girlProfile} alt="profile pic" className='w-full h-full object-cover rounded-full' />
+    </div>
+  )
+
   return (
     <div className="h-full w-fit flex flex-col bg-white rounded-3xl">
       <div className="border-b border-slate-100 p-5">
@@ -17,22 +34,45 @@ const Sidebar = () => {
           <IconButton icon={SlCloudUpload} />
         </div>
       </div>
-      <div>
-        <div className="border border-slate-100 flex justify-between items-center px-5 py-3">
-          <h3 className="text-xs font-medium text-neutral-600">Uploaded documents</h3>
-          <button className="text-blue-600 hover:text-blue-800 text-sm">Clear All</button>
+      <div className='flex-col w-full h-fit'>
+        <div>
+          <div className="border border-slate-100 flex justify-between items-center px-5 py-3">
+            <h3 className="text-xs font-medium text-neutral-600">Uploaded documents</h3>
+            <button className="text-blue-600 hover:text-blue-800 text-xs">Clear All</button>
+          </div>
+          <div className='h-35 flex-col ps-2 py-2 overflow-y-auto no-scrollbar'>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+            <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+          </div>
         </div>
-        <div className='max-h-1/2 flex-col ps-2 py-2 overflow-y-auto no-scrollbar'>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
-          <UploadedDoc text="Hypothalamus.pdf" onDelete={() => {}}/>
+        <div>
+          <div className="border border-slate-100 flex justify-between items-center px-5 py-3">
+            <h3 className="text-xs font-medium text-neutral-600">Your conversations</h3>
+            <button className="text-blue-600 hover:text-blue-800 text-xs">Clear All</button>
+          </div>
+          <div className='h-35 flex-col ps-2 py-2 overflow-y-auto no-scrollbar'>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+            <PreviousChat text="What is Hypothalamus?" onDelete={() => {}}/>
+          </div>
         </div>
+      </div>
+      <div className='h-full flex gap-2 flex-col p-1'>
+        <SecondaryButton text="Settings" icon={settingsIcon}/>
+        <SecondaryButton text="Lazy Mango" icon={profilePic}/>
       </div>
     </div>
   );
