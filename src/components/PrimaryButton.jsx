df1b3d5752aa06a3bfc27b9121329795d480f8c8
ajk0x00 <ajk0x00@gmail.com>
2025-08-29 05:09:11 +0000

    Updated text size and component size of PrimaryButton.

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -4,11 +4,11 @@ const PrimaryButton = ({ text, icon: Icon, ...props }) => {
 
   return (
     <button 
-      className='flex items-center justify-center gap-2 px-2 py-3 bg-indigo-500 text-white rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-200 ease-in-out w-55'
+      className='flex items-center justify-center gap-3 px-2 py-3 bg-indigo-500 text-white rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 transition duration-200 ease-in-out w-45'
       {...props}
     >
       {Icon && <Icon />}
-      <span className='font-semibold'>{text}</span>
+      <span className='text-sm font-semibold'>{text}</span>
     </button>
   );
 };
