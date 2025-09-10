18f33e4c84291dfe4c813ab1ae02159660e7bbbe
ajk0x00 <ajk0x00@gmail.com>
2025-09-10 04:46:44 +0000

    Add gradient effect on uploaded docs section.

--- src/fragments/Sidebar.jsx
+++ src/fragments/Sidebar.jsx
@@ -36,7 +36,7 @@ const Sidebar = () => {
           <DocUploadButton icon={SlCloudUpload} onChange={handleDocUpload}/>
         </div>
       </div>
-      <div className='h-5/14 flex-grow-1'>
+      <div className='relative h-5/14 flex-grow-1'>
         <div className="h-fit flex justify-between items-center py-3">
             <h3 className="text-xs font-medium text-neutral-600">Uploaded documents</h3>
             <button className={`${uploadedDocs.length > 0 ? 'text-blue-600 hover:text-blue-800' : 'text-neutral-400'} text-xs`} disabled={uploadedDocs.length == 0}>Clear All</button>
@@ -46,6 +46,7 @@ const Sidebar = () => {
             <UploadedDoc key={index} text={doc} onDelete={() => {}}/>
           ))}
         </div>
+        <div className='absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-white to-transparent'></div>
       </div>
       <div className='relative h-5/14 flex-grow-1'>
         <div className="h-fit flex justify-between items-center px-5 py-3">
